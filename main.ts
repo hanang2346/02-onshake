1e80b5a95ad159ec7a679a14ce2f02c8ea98cf3e
hanang2346 <77667576+hanang2346@users.noreply.github.com>
2021-02-12 22:55:40 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+let dice = 0
+input.onGesture(Gesture.Shake, function () {
+    dice = randint(0, 5)
+    if (dice == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dice == 1) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dice == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dice == 3) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (dice == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
